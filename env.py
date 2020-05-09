8cb2929d75009d7205794284ce858e98119672b4
Luc Gotink <L.h.gotink@gmail.com>
2020-05-09 16:40:26 +0000

    Connection to mongoDB, show_menu fucntion, and halfway through main_loop function

---
+++ env.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ["SECREY_KEY"] = "Tamaguchi1"
+
+os.environ["MONGO_URI"] = "mongodb+srv://DeclineOfMind:5thGreatape@myfirstcluster-f87ys.mongodb.net/MyTestDB?retryWrites=true&w=majority"
+
+os.environ["MONGO_DBNAME"] = "MyTestDB"
